996d2638db7f74658391c5a5b36bd42b89e17f86
Andrii Baidachenko <andrewbayd@gmail.com>
2022-04-27 22:21:35 +0200

    Add iterative solution for BST LCA

--- src/com/andrewbayd/BinarySearchTreeLCA/Solution.java
+++ src/com/andrewbayd/BinarySearchTreeLCA/Solution.java
@@ -1,12 +1,31 @@
 package com.andrewbayd.BinarySearchTreeLCA;
 
+/*
+Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
+
+https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+ */
+
 public class Solution {
 
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    public TreeNode lowestCommonAncestorIterative(TreeNode root, TreeNode p, TreeNode q) {
+        while (root != null) {
+            if (p.val < root.val && q.val < root.val) {
+                root = root.left;
+            } else if (p.val > root.val && q.val > root.val) {
+                root = root.right;
+            } else {
+                return root;
+            }
+        }
+        return root;
+    }
+
+    public TreeNode lowestCommonAncestorRecursive(TreeNode root, TreeNode p, TreeNode q) {
         if (p.val > root.val && q.val > root.val) {
-            return lowestCommonAncestor(root.right, p, q);
+            return lowestCommonAncestorRecursive(root.right, p, q);
         } else if (p.val < root.val && q.val < root.val) {
-            return lowestCommonAncestor(root.left, p, q);
+            return lowestCommonAncestorRecursive(root.left, p, q);
         } else {
             return root;
         }
